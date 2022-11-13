11d14e90455274d96431d6309add6d5ffa6c2bbd
Pratush Rai <pratushrai0309@gmail.com>
2022-11-13 18:59:16 +0530

    Command

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,42 @@
+use std::{fs::File, io::Read};
+
 use toml::Value;
 use serde_derive::Deserialize;
 
 
 #[derive(Debug, Deserialize)]
-struct Command{
-  alias: String,
-  program: String,
-  args: String
+pub struct Command{
+  pub alias: String,
+  pub program: String,
+  pub args: String
 }
 
 impl Command{
-  fn parse_toml(&self, path: &str){
+  pub fn new() -> Self{
+    Self{
+      alias: String::new(),
+      program: String::new(),
+      args: String::new()
+    }
+  }
+  pub fn parse_toml(&self, path: &str){
     let mut commands: Vec<Command> = Vec::new();
+    let mut command: Option<Command> = None;
+    let mut file = File::open(path).expect("Unable to open file");
+    let mut buf: String = String::new();
+    file.read_to_string(&mut buf);
+    let cmd_data:Vec<String> = buf.split("[Command]").map(|x| {
+      x.trim().to_owned()
+    }).collect();
+    for cmd in cmd_data{
+      if cmd == ""{
+        continue;
+      }
+      else{
+        command = Some(toml::from_str(&cmd).unwrap());
+        commands.push(command.unwrap())
+      }
+    }
+    println!("{:#?}", commands)
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,9 @@
 
 mod commands;
 
-use toml::Value;
-use serde_derive::Deserialize;
-
-use std::{fs::File, io::Read};
-
-#[derive(Debug, Deserialize)]
-struct Command{
-    alias: String,
-    program: String,
-    args: String
-}
+use commands::Command;
 
 fn main() {
-    let mut commands: Vec<Command> = Vec::new();
-    let mut command: Option<Command> = None;
-    let mut file = File::open("/home/pratush/Documents/cli-unnamed/src/test.toml").expect("Unable to find file");
-    let mut buf: String = String::new();
-    file.read_to_string(&mut buf).unwrap();
-    let main_data:Vec<String> = buf.split("[Command]").map(|s|{
-        s.trim().to_owned()
-    }).collect();
-    //println!("{:#?}", main_data);
-    for i in main_data{
-        if i  == ""{
-            continue;
-        }
-        else{
-           let command :Option<Command> = Some(toml::from_str(&i).unwrap()); 
-           commands.push(command.unwrap()) 
-        }
-    }
-    println!("{:#?}", commands);
-    // // println!("{:#?}", main_data);
-    // let command: Command = toml::from_str(r#"
-    // alias = "gitadd"
-    // program = "git"
-    // args = "add ."
-    // "#).expect("Unable to parse");
-    // println!("{:#?}", command);
-}
-
-#[test]
-fn it_works(){
-    let command: Command = toml::from_str(r#"
-    alias = "c"
-    program = "code"
-    args = "."
-    "#).expect("Unable to parse");
-
-    assert_eq!(command.alias, "c");
-    assert_eq!(command.program, "code");
-    assert_eq!(command.args, ".")
+   let command = Command::new();
+   command.parse_toml("/home/pratush/Documents/cli-unnamed/src/test.toml") 
 }
